582d77a59bd2c5372bab86094cd59112f190d086
Phil Jackson <phil@shellarchive.co.uk>
2016-11-24 16:31:00 +0000

    Tidy up the project file.

--- project.clj
+++ project.clj
@@ -18,11 +18,9 @@
                  [hiccup "1.0.5"]
                  [cljsjs/react-select "1.0.0-rc.1"]
                  [yogthos/config "0.8"]
-                 [org.clojure/clojurescript "1.9.229"
-                  :scope "provided"]
+                 [org.clojure/clojurescript "1.9.229" :scope "provided"]
                  [secretary "1.2.3"]
-                 [venantius/accountant "0.1.7"
-                  :exclusions [org.clojure/tools.reader]]]
+                 [venantius/accountant "0.1.7" :exclusions [org.clojure/tools.reader]]]
 
   :plugins [[lein-environ "1.0.2"]
             [lein-cljsbuild "1.1.1"]
@@ -53,66 +51,43 @@
   :cljsbuild
   {:builds {:min
             {:source-paths ["src/cljs" "src/cljc" "env/prod/cljs"]
-             :compiler
-             {:output-to "target/cljsbuild/public/js/app.js"
-              :output-dir "target/uberjar"
-              :optimizations :advanced
-              :pretty-print  false}}
-            :app
-            {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-             :compiler
-             {:main "wt.dev"
-              :asset-path "/js/out"
-              :output-to "target/cljsbuild/public/js/app.js"
-              :output-dir "target/cljsbuild/public/js/out"
-              :source-map true
-              :optimizations :none
-              :pretty-print  true}}
-
-
-
-            }
-   }
-
-
-  :figwheel
-  {:http-server-root "public"
-   :server-port 3449
-   :nrepl-port 7002
-   :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"
-                      "cider.nrepl/cider-middleware"
-                      "refactor-nrepl.middleware/wrap-refactor"
-                      ]
-   :css-dirs ["resources/public/css"]
-   :ring-handler wt.handler/app}
-
-
-
+             :compiler {:output-to "target/cljsbuild/public/js/app.js"
+                        :output-dir "target/uberjar"
+                        :optimizations :advanced
+                        :pretty-print  false}}
+            :app {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
+                  :compiler {:main "wt.dev"
+                             :asset-path "/js/out"
+                             :output-to "target/cljsbuild/public/js/app.js"
+                             :output-dir "target/cljsbuild/public/js/out"
+                             :source-map true
+                             :optimizations :none
+                             :pretty-print  true}}}}
+  :figwheel {:http-server-root "public"
+             :server-port 3449
+             :nrepl-port 7002
+             :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"
+                                "cider.nrepl/cider-middleware"
+                                "refactor-nrepl.middleware/wrap-refactor"]
+             :css-dirs ["resources/public/css"]
+             :ring-handler wt.handler/app}
   :profiles {:dev {:repl-options {:init-ns wt.repl}
-
                    :dependencies [[ring/ring-mock "0.3.0"]
                                   [ring/ring-devel "1.5.0"]
                                   [prone "1.1.2"]
                                   [figwheel-sidecar "0.5.7"]
                                   [org.clojure/tools.nrepl "0.2.12"]
                                   [com.cemerick/piggieback "0.2.2-SNAPSHOT"]
-                                  [pjstadig/humane-test-output "0.8.1"]
-                                  ]
-
+                                  [pjstadig/humane-test-output "0.8.1"]]
                    :source-paths ["env/dev/clj"]
                    :plugins [[lein-figwheel "0.5.7"]
                              [cider/cider-nrepl "0.10.0-SNAPSHOT"]
                              [org.clojure/tools.namespace "0.3.0-alpha2"
                               :exclusions [org.clojure/tools.reader]]
-                             [refactor-nrepl "2.0.0-SNAPSHOT"
-                              :exclusions [org.clojure/clojure]]
-                             ]
-
+                             [refactor-nrepl "2.0.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]
-
                    :env {:dev true}}
-
              :uberjar {:hooks [minify-assets.plugin/hooks]
                        :source-paths ["env/prod/clj"]
                        :prep-tasks ["compile" ["cljsbuild" "once" "min"]]
