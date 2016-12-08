102a07d5ec0c351561548dc707f97cd83e456991
Phil Jackson <phil@shellarchive.co.uk>
2016-12-08 17:14:54 +0000

    Initial checkin.

---
+++ bin/deploy.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+# delete old version
+rm -rf tmp
+
+# create our structure if need be
+mkdir -p js
+
+# clone ourselves
+git clone . tmp --branch "master"
+pushd tmp
+
+# build
+lein cljsbuild once min
+
+# copy the new js
+popd
+cp tmp/dist/js/app.js js
+
+cp -r tmp/resources/public/css .
+cp -r tmp/resources/public/tz .
+
+git add css tz js
