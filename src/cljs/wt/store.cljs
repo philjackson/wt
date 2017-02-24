d2042ec6f46f37940ad22199966f5771bb144422
Phil Jackson <phil@shellarchive.co.uk>
2017-02-24 07:59:12 +0000

    Delete the old store file.

--- src/cljs/wt/store.cljs
+++
@@ -1,8 +0,0 @@
-(ns wt.store)
-
-(defn from-ls [key]
-  (some->> (.getItem (.-localStorage js/window) key)
-           (cljs.reader/read-string)))
-
-(defn to-ls [key value]
-  (.setItem (.-localStorage js/window) key (str value)))
